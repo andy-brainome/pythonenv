aa8a533f4ffe1e37dc9172aabb3b2d60e6ebb4ad
Andy Stevko <andy.stevko+github@brainome.ai>
2020-05-30 11:44:48 -0700

    running standalone within container

--- sys-path.sh
+++ sys-path.sh
@@ -4,5 +4,6 @@ NUITKASYSTEMPATH=`./sys-path.bin`
 python3 -m nuitka --version
 echo "native system path: $SYSTEMPATH"
 echo "nuitka system path: $NUITKASYSTEMPATH"
+echo "standalone path:" `./syspath.dist/sys-path`
 echo "::set-output name=systempath::$SYSTEMPATH"
 echo "::set-output name=nuitkasystempath::$NUITKASYSTEMPATH"
