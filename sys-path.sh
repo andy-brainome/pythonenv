dea50eaa51cdd0951888d177de8bb305147b0aa8
Andy Stevko <andy.stevko+github@brainome.ai>
2020-05-30 11:55:26 -0700

    trying to enable standalone

--- sys-path.sh
+++ sys-path.sh
@@ -4,6 +4,6 @@ NUITKASYSTEMPATH=`./sys-path.bin`
 python3 -m nuitka --version
 echo "native system path: $SYSTEMPATH"
 echo "nuitka system path: $NUITKASYSTEMPATH"
-echo "standalone path:" `./syspath.dist/sys-path`
+echo "standalone path:" `./sys-path.dist/sys-path`
 echo "::set-output name=systempath::$SYSTEMPATH"
 echo "::set-output name=nuitkasystempath::$NUITKASYSTEMPATH"
