2e941538eda3db4ab7f6fe37a86f9fddcb425083
Andy Stevko <andy.stevko+github@brainome.ai>
2020-05-30 11:30:34 -0700

    refine documentation; try standalone

--- sys-path.sh
+++ sys-path.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SYSTEMPATH=`python3 sys-path.py`
 NUITKASYSTEMPATH=`./sys-path.bin`
+python3 -m nuitka --version
 echo "native system path: $SYSTEMPATH"
 echo "nuitka system path: $NUITKASYSTEMPATH"
 echo "::set-output name=systempath::$SYSTEMPATH"
